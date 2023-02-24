f59bdf9c950c88b1867587bb5f02c2135a546c86
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-24 22:41:58 +0100

    front

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello, world!</div>;
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <input type="text" placeholder="Items" />
+      <button>Add ToDo</button>
+    </div>
+  );
 }
 
 export default App;
